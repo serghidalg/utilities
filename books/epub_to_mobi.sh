7d9982bfe4897934ec0694aefa42da31c2ef8734
Sergio Hidalgo <serghidalg@protonmail.com>
2022-04-25 16:12:17 +0200

    More clear outputs

--- books/epub_to_mobi.sh
+++ books/epub_to_mobi.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "#############################################################"
+echo "# Converting all your epub files to mobi"
+echo "#############################################################"
+
+
 for book in *.epub; do echo "Converting $book"; ebook-convert "$book" "$(basename "$book" .epub).mobi"; done
 mkdir mobi
 mv *.mobi mobi/
--- books/pdf_to_mobi.sh
+++ books/pdf_to_mobi.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+echo "#############################################################"
+echo "# You are converting all your pdfs to mobi"
+echo "#############################################################"
 for book in *.pdf; do echo "Converting $book"; ebook-convert "$book" "$(basename "$book" .pdf).mobi"; done
 mkdir mobi
 mv *.mobi mobi/
