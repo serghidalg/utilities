5f8e08c534d268300f2645e24dcd277ecaf33ec0
Sergio Hidalgo <serghidalg@protonmail.com>
2022-04-29 16:43:12 +0200

    Commentary to tune parameters

--- books/pdf_to_pdf.sh
+++ books/pdf_to_pdf.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #sudo apt-get install texlive-extra-utils
 for book in *.pdf; do echo "Converting $book"; pdfcrop "$book" "$(basename "$book" .pdf)_transform.pdf"; mv "$(basename "$book" .pdf)_transform.pdf" mobi/"$(basename "$book" .pdf)_transform.pdf"; done
+#add the following parameters to tune left-top-right-bot margins specifically pdfcrop --margins '0 0 0 -30'
